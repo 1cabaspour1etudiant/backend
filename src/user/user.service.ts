73156eb4fdc670f85fccd0813161194b3538b991
kecsou <kevin.voyer.developpeur@gmail.com>
2021-03-13 16:26:42 +0100

    feat: hash user password using bcrypt

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -3,9 +3,13 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { CreateUserDto } from './dto/create-user-dto';
 import { User } from './entities/user.entity';
+import { hash } from 'bcrypt';
+
 
 @Injectable()
 export class UserService {
+    private readonly saltRounds = 10;
+
     constructor(
         @InjectRepository(User)
         private readonly userRespository: Repository<User>
@@ -19,7 +23,13 @@ export class UserService {
             throw new ConflictException(`This email adress is already use`);
         }
 
-        user = await this.userRespository.create(createUserDto);
+        const passwordHashed = await hash(createUserDto.password, this.saltRounds);
+
+        user = await this.userRespository.create({
+            ...createUserDto,
+            password: passwordHashed,
+        });
+
         return this.userRespository.save(user);
     }
 }
