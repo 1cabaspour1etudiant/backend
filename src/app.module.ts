bde67d8f11363028b9fdba1833e08650478a70e2
kecsou <kevin.voyer.developpeur@gmail.com>
2021-03-13 17:19:12 +0100

    feat: use nestjs/config rather than dotenv

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,26 +1,35 @@
+import * as Joi from 'joi';
 import { Module } from '@nestjs/common';
-import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 
-const port = Number(process.env.POSTGRES_PORT);
-
-const typeOrmOptions:TypeOrmModuleOptions = {
-  type: 'postgres',
-  host: process.env.POSTGRES_HOST,
-  port,
-  username: process.env.POSTGRES_USERNAME,
-  password: process.env.POSTGRES_PASSWORD,
-  database: process.env.POSTGRES_DATABASE,
-  autoLoadEntities: true,
-  synchronize: process.env.PRODUCTION === 'false',
-};
-
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      validationSchema: Joi.object({
+        PRODUCTION: Joi.boolean(),
+        POSTGRES_HOST: Joi.required(),
+        POSTGRES_PORT: Joi.number().default(5432),
+        POSTGRES_USERNAME: Joi.string().required(),
+        POSTGRES_PASSWORD: Joi.required(),
+        POSTGRES_DATABASE: Joi.required(),
+      }),
+    }),
     UserModule,
-    TypeOrmModule.forRoot(typeOrmOptions)
+    TypeOrmModule.forRoot({
+        type: 'postgres',
+        host: process.env.POSTGRES_HOST,
+        port: Number(process.env.POSTGRES_PORT),
+        username: process.env.POSTGRES_USERNAME,
+        password: process.env.POSTGRES_PASSWORD,
+        database: process.env.POSTGRES_DATABASE,
+        autoLoadEntities: true,
+        synchronize: process.env.PRODUCTION === 'false',
+      }
+    )
   ],
   controllers: [AppController],
   providers: [AppService],
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,6 @@
-import { config } from 'dotenv';
-config();
-
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
