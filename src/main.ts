9945732a5eb24fc9d534e560184e753ce562e534
kecsou <kevin.voyer.developpeur@gmail.com>
2021-03-13 14:47:57 +0100

    fix: load config before loading modules

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
+import { config } from 'dotenv';
+config();
+
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { config } from 'dotenv';
 
-config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
