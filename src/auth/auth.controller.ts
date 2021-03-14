1683071403d3fb3a91cf64a01bcbca7d8522607f
kecsou <kevin.voyer.developpeur@gmail.com>
2021-03-14 20:41:30 +0100

    fix: guard calling

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { User } from 'src/user/entities/user.entity';
+import { AuthService } from './auth.service';
+import { GetUser } from './decorators/get-user.decorator';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+    constructor(private readonly authService: AuthService) {}
+
+    @UseGuards(AuthGuard('local'))
+    @Post('login')
+    async login(@GetUser() user:User) {
+        return this.authService.login(user);
+    }
+}
