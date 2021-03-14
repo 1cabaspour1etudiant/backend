405cb95139b76e938974d58f2b1bea6ad820c9c8
kecsou <kevin.voyer.developpeur@gmail.com>
2021-03-14 19:04:36 +0100

    feat: import Passport module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
-import { JwtModule, JwtService } from '@nestjs/jwt';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
 import { UserModule } from 'src/user/user.module';
 import { AuthService } from './auth.service';
 
 @Module({
   imports: [
     UserModule,
+    PassportModule.register({ defaultStrategy: 'jwt' }),
     JwtModule.register({
       secret: process.env.JWT_SECRET,
       signOptions: { expiresIn: '3600s' }
