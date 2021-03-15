29799293cb116ac95cdd3ff32a2f44eadc1a5c34
kecsou <kevin.voyer.developpeur@gmail.com>
2021-03-15 01:27:02 +0100

    docs: add swagger documentation

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,19 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('1 cabas pour un etudiant')
+    .setDescription("L'api du service un cabas pour un etudiant")
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
--- src/user/dto/create-user-dto.ts
+++ src/user/dto/create-user-dto.ts
@@ -1,35 +1,46 @@
 import { IsEmail, IsString, Length } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateUserDto {
+    @ApiProperty()
     @IsString()
     firstname: string;
 
+    @ApiProperty()
     @IsString()
     lastname: string;
 
+    @ApiProperty()
     @IsString()
     tel: string;
 
+    @ApiProperty()
     @IsString()
     @IsEmail()
     email: string;
 
+    @ApiProperty()
     @IsString()
     @Length(6, 32)
     password: string;
 
+    @ApiProperty()
     @IsString()
     adress: string;
 
+    @ApiProperty()
     @IsString()
     city: string;
 
+    @ApiProperty()
     @IsString()
     postalCode: string;
 
+    @ApiProperty()
     @IsString()
     role:string;
 
+    @ApiProperty()
     @IsString()
     activity: string;
 }
