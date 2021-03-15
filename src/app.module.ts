2b512ab7348dddaf93c4ff1db0bbf93fab41a32b
kecsou <kevin.voyer.developpeur@gmail.com>
2021-03-15 01:45:35 +0100

    refacto: remove unuse app controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,6 @@ import * as Joi from 'joi';
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
-import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
@@ -34,7 +33,6 @@ import { AuthModule } from './auth/auth.module';
     ),
     AuthModule
   ],
-  controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
