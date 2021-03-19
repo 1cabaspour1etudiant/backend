779a5b02a3530f0687027dd5bcf61f3b60af70f2
kecsou <kevin.voyer.developpeur@gmail.com>
2021-03-19 19:21:50 +0100

    feat: use addresse entity

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -3,13 +3,14 @@ import { JwtModule } from '@nestjs/jwt';
 import { MulterModule } from '@nestjs/platform-express';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from 'src/auth/auth.module';
+import { Address } from './entities/address.entity';
 import { User } from './entities/user.entity';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([User]),
+    TypeOrmModule.forFeature([User, Address]),
     forwardRef(() => AuthModule),
     JwtModule.register({
       secret: process.env.JWT_SECRET,
