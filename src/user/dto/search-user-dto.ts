5047aa85d0d876c41468e414a06ecf2420155211
kecsou <kevin.voyer.developpeur@gmail.com>
2021-03-22 00:50:14 +0100

    fix: filters search user dto

--- src/user/dto/search-user-dto.ts
+++ src/user/dto/search-user-dto.ts
@@ -1,17 +1,19 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsInt, IsOptional, IsPositive, Max, Min, } from 'class-validator';
-
+import { IsInt, IsOptional, Max, Min } from 'class-validator';
+import { Transform } from 'class-transformer';
 
 export class SearchUserDto {
     @ApiProperty()
     @IsOptional()
     @IsInt()
     @Min(0)
+    @Transform(({ value }) => parseInt(value))
     page: number = 0;
 
     @ApiProperty()
     @IsOptional()
-    @IsPositive()
+    @Min(1)
     @Max(20)
+    @Transform(({ value }) => parseInt(value))
     pageSize: number = 20;
 }
