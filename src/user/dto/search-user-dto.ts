beb91dc13e70eb7c818772e1d5075d2d2c3ad2af
kecsou <kevin.voyer.developpeur@gmail.com>
2021-03-24 00:24:50 +0100

    refactor: extends from PaginationDto

--- src/user/dto/search-user-dto.ts
+++ src/user/dto/search-user-dto.ts
@@ -1,19 +1,3 @@
-import { ApiProperty } from '@nestjs/swagger';
-import { IsInt, IsOptional, Max, Min } from 'class-validator';
-import { Transform } from 'class-transformer';
+import { PaginationDto } from './abstract-pagination-dto';
 
-export class SearchUserDto {
-    @ApiProperty()
-    @IsOptional()
-    @IsInt()
-    @Min(0)
-    @Transform(({ value }) => parseInt(value))
-    page: number = 0;
-
-    @ApiProperty()
-    @IsOptional()
-    @Min(1)
-    @Max(20)
-    @Transform(({ value }) => parseInt(value))
-    pageSize: number = 20;
-}
+export class SearchUserDto extends PaginationDto {}
