ee0a8f9b658252ceddb337eef420f062b065b8c3
kecsou <kevin.voyer.developpeur@gmail.com>
2021-03-24 21:26:23 +0100

    feat: add type for get sponsorship requests dto

--- src/user/dto/get-sponsorship-requests-dto.ts
+++ src/user/dto/get-sponsorship-requests-dto.ts
@@ -1,3 +1,10 @@
+import { ApiProperty } from "@nestjs/swagger";
+import { IsIn, IsString } from "class-validator";
 import { PaginationDto } from "./abstract-pagination-dto";
 
-export class GetSponsorshipRequestsDto extends PaginationDto {}
+export class GetSponsorshipRequestsDto extends PaginationDto {
+    @ApiProperty()
+    @IsString()
+    @IsIn(['received', 'sent'])
+    type: string = 'received';
+}
